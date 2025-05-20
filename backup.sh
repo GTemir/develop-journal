c9c29bcf94bb30e8ba327e685ee2f4b7603f7848
ginux <rytheed@gmail.com>
2025-05-20 11:47:46 +0500

    new branch

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-SOURCE_DIR=/home/core/Documents
+BACK_DIR=/home/core/Documents
 BACKUP_DIR=/home/core/devops-journal/backupD
 DATE=$(date +"%Y-%m-%d-%H-%M")
 ARCHIVE_NAME="dockuments_backup_$DATE.tar.gz"
 mkdir -p "$BACKUP_DIR"
-tar -czf "$BACKUP_DIR/$ARCHIVE_NAME" -C "$SOURCE_DIR" .
+tar -czf "$BACKUP_DIR/$ARCHIVE_NAME" -C "$BACK_DIR" .
 echo "Архив создан: $BACKUP_DIR/$ARCHIVE_NAME"
